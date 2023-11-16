f7f30e1b87dc1ff9ead0a4f9761717e0bbb6efeb
junioranheu <junioranheu@gmail.com>
2023-11-16 12:11:32 -0300

    implementar Post/DeletarArquivosEmPasta

--- @junioranheu-utils-package/Fixtures/Post.cs
+++ @junioranheu-utils-package/Fixtures/Post.cs
@@ -130,6 +130,59 @@ namespace junioranheu_utils_package.Fixtures
         }
 
         /// <summary>
+        /// Deleta os arquivos de uma pasta;
+        /// 
+        /// - Todos os arquivos;
+        /// DeletarArquivosEmPasta(path);
+        ///
+        /// - Extensões específicas;
+        /// List<string> extensoesEspecificas = [".txt", ".mp4"];
+        /// DeletarArquivosEmPasta(path, listaExtensoes: extensoesEspecificas);
+        ///
+        /// - Nomes específicos;
+        /// List<string> nomesEspeficos = ["ola", "tmr_pes"];
+        /// DeletarArquivosEmPasta(path, listaNomes: nomesEspeficos);
+        /// </summary>
+        public static bool DeletarArquivosEmPasta(string path, string webRootPath, List<string>? listaExtensoes = null, List<string>? listaNomes = null, bool? ignorarExtensaoEmListaNomes = true)
+        {
+            string fullPath = Path.Combine(webRootPath, path);
+
+            if (Directory.Exists(fullPath))
+            {
+                string[] files;
+
+                if (listaExtensoes is not null && listaExtensoes.Count > 0)
+                {
+                    files = Directory.GetFiles(fullPath).Where(x => listaExtensoes.Any(extensao => x.EndsWith(extensao, StringComparison.OrdinalIgnoreCase))).ToArray();
+                }
+                else if (listaNomes is not null && listaNomes.Count > 0)
+                {
+                    if (ignorarExtensaoEmListaNomes.GetValueOrDefault())
+                    {
+                        files = Directory.GetFiles(fullPath).Where(file => listaNomes.Contains(Path.GetFileNameWithoutExtension(file), StringComparer.OrdinalIgnoreCase)).ToArray();
+                    }
+                    else
+                    {
+                        files = Directory.GetFiles(fullPath).Where(x => listaNomes.Contains(Path.GetFileName(x), StringComparer.OrdinalIgnoreCase)).ToArray();
+                    }
+                }
+                else
+                {
+                    files = Directory.GetFiles(fullPath);
+                }
+
+                foreach (string file in files)
+                {
+                    File.Delete(file);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
         /// Exemplo de streaming de um arquivo dividido em chunks;
         /// 
         /// Sites para testar/validar os chunks gerados:
